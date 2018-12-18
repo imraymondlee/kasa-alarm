6865cb02fd49024acbeaf4bbb1b4d38166b91dec
imraymondlee <l.leeraymond@gmail.com>
2018-12-17 21:29:49 -0800

    Created express route

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const { Client } = require('tplink-smarthome-api');
 
 
-const startingBrightness = 10;
+const startingBrightness = 0;
 const endingBrightness = 100;
 
 // Increment every 10 seconds with 10 brightness
@@ -16,11 +16,26 @@ const endAlarm = 50;
 let currentAlarm = startAlarm;
 let currentBrightness = startingBrightness;
 
+let app = express();
+
+app.get('/', (req, res) => {
+	res.send('Hello World!');
+});
+
+
+// Input wake up time. 
+// Start time will start 30 minutes prior to wake up time.
+// Every 3 minutes, will increase by 10 for 30 minutes.
+app.get('/set-alarm', (req, res) => {
+
+	updateTime();
+	res.send('Alarm Set!');
+});
+
+
 const client = new Client();
 
 bulb = client.getBulb({host: '192.168.1.137'});
-
-
 
 const updateTime = () => {
 	let date = new Date();
@@ -52,4 +67,6 @@ const updateTime = () => {
 	}
 }
 
-updateTime();
+app.listen(3000, () => {
+	console.log('Server started on port 3000.')
+})
